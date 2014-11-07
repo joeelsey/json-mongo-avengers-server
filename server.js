25a0c0154cd316581c5b6fbf1e7e167b2c0a73be
Joe Elsey <joe.elsey@gmail.com>
2014-11-07 15:58:07 -0800

    svr chg

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ var app = express();
 var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://ec2-54-83-204-78.compute-1.amazonaws.com
-/d8k48gen4uded8');
+/avengers_dev');
+
+//mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/avengers_dev')
 
 require('./routes/avenger_routes')(app);
 
+//app.set('port', 3000);
 app.set('port', 5432);
 app.listen(app.get('port'), function() {
   console.log('server running on port: ' + app.get('port'));
