def238e29db83d6ce2200d1b4f74895f08f54579
Joe Elsey <joe.elsey@gmail.com>
2014-11-07 17:34:09 -0800

    server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var mongoose = require('mongoose');
 var pg = require('pg');
 
-var url = process.env.MONGOHQ_URL || 'mongodb://ec2-54-243-42-236.compute-1.amazonaws.com/ddrq6viktq67e9';
+var url = process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://ec2-54-243-42-236.compute-1.amazonaws.com/ddrq6viktq67e9';
 mongoose.connect(url);
 
 //mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/avengers_dev');
