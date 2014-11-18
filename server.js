1650504c9ac5470f92a634439bdbf90437561342
Joe Elsey <joe.elsey@gmail.com>
2014-11-18 15:59:49 -0800

    sml updates

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
-var pg = require('pg');
 
-var url = process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://ec2-54-243-42-236.compute-1.amazonaws.com/ddrq6viktq67e9';
-mongoose.connect(url);
 
-///mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/avengers_dev');
+// var url = process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost:3000';
+// mongoose.connect(url);
+
+//this connect is for local testing.
+mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/avengers_dev');
 
 require('./routes/avenger_routes')(app);
 
-//app.set('port',process.env.PORT || 3000);
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function() {
   console.log('server running on port: ' + app.get('port'));
